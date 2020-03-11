c0286221730b408003c86f2d2f0970a9bccffa29
Bada1208 <sysoievbogdan@gmail.com>
2020-03-11 05:33:41 +0100

    lesson1

---
+++ src/main/java/com/sysoiev/spring_core/App.java
@@ -0,0 +1,20 @@
+package com.sysoiev.spring_core;
+
+public class App {
+    Client client;
+    ConsoleEventLogger eventLogger;
+
+    private void logEvent(String msg) {
+        String message = msg.replaceAll(client.getId(), client.getFullName());
+        eventLogger.logEvent(msg);
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.client = new Client("1", "John Smith");
+        app.eventLogger = new ConsoleEventLogger();
+
+        app.logEvent("Some event for user 1");
+
+    }
+}
---
+++ src/main/java/com/sysoiev/spring_core/ConsoleEventLogger.java
@@ -0,0 +1,7 @@
+package com.sysoiev.spring_core;
+
+public class ConsoleEventLogger {
+    public  void logEvent(String msg){
+        System.out.println(msg);
+    }
+}
